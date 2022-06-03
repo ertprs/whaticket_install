9c4e3112b4df807b75cc363b99936dfa0ba9d5b0
jerbison <105988682+jerbison@users.noreply.github.com>
2022-06-03 14:49:47 -0400

    Update _app.sh

--- variables/_app.sh
+++ variables/_app.sh
@@ -7,7 +7,7 @@
 jwt_secret=$(openssl rand -base64 32)
 jwt_refresh_secret=$(openssl rand -base64 32)
 
-deploy_password=@Master007
+Sistemas_password=@Master007
 
 mysql_root_password=Master007
 
@@ -16,4 +16,4 @@ db_pass=$(openssl rand -base64 32)
 db_user=$(openssl rand -base64 32)
 db_name=$(openssl rand -base64 32)
 
-deploy_email=jerbison.ti@gmail.com
+Sistemas_email=jerbison.ti@gmail.com
